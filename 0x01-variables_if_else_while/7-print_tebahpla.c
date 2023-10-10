504d61f15b40e299e417e8fb17f3e4ae8ccb8137
“vintage254” <derricknjuguna414@gmail.com>
2023-10-10 10:59:18 -0700

    betty changes

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-/* This is the main function.
-   It prints the lowercase alphabet in reverse order. */
-int main(void)
+/*
+ *This is the main function.
+  *It prints the lowercase alphabet in reverse order.
+  */
+int main(void)/*this is the main function */
 {
 	char ch;
 
