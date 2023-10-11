42ea4dd91d8a8f3dfd800e7b617282716e79a948
“vintage254” <derricknjuguna414@gmail.com>
2023-10-11 12:31:23 -0700

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+include"main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ *
+ * return (0)
+ */
+void print_alphabet(void)
+{
+	char l
+		for(l= 'a';l <= 'z' ; l++)
+			_putchar(l);
+	putchar("/n");
+}
