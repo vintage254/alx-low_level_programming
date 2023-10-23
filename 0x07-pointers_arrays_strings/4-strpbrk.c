965aaf6b6ccad9c2de8c4bd42950dcbee56ee1da
“vintage254” <derricknjuguna414@gmail.com>
2023-10-23 16:57:45 -0700

    finishing up on mandatory tasks

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * _strpbrk - main function of the prototype
+  * _strpbrk - main function of the prototype
   *
   * @s: function parameter
   *
@@ -8,18 +9,18 @@
   *
   * Return: Always 0.
   */
+
 char *_strpbrk(char *s, char *accept)
 {
-	int f;
-	int x;
+	int k, j;
 
-	for (f = 0; s[f] != '\0'; f++)
+	for (k = 0; s[k] != '\0'; k++)
 	{
-		for (x= 0; accept[x] != '\0'; x++)
-		{
-			if(s[f] == accept[x])
-				return(s + f);
-		}
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+	if (s[k] == accept[j])
+	return (s + k);
+	}
 	}
 	return (0);
 }
