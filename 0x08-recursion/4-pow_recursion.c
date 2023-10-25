c63940c7364e4a7e684c822e455089b00a8de81f
“vintage254” <derricknjuguna414@gmail.com>
2023-10-25 14:34:41 -0700

    betty corrections

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,19 +5,19 @@
   * @y: parameter 2
   * Return: value of a given number passed to the function
   */
-
-
 int _pow_recursion(int x, int y)
 {
-
-if (y < 0)
-        return (-1);
-
-else if (y == 0)
-        return (1);
-
-else
-        return (x * _pow_recursion(x, (y - 1)));
-
-return (0);
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, (y - 1)));
+	}
+	return (0);
 }
