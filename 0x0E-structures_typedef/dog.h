ca75055ce8ce4c460ef49da1ad676a0f1264bd6d
“vintage254” <derricknjuguna414@gmail.com>
2023-11-06 05:44:41 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef  DOG_H 
+#define  DOG_H
+
+/**
+ * @stuct dog - new type containing char* name float age char* owner
+ * Return : o
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
