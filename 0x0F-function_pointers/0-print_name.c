b8fc6c4d8b7fde5475430f423718371b8f3abe11
“vintage254” <derricknjuguna414@gmail.com>
2023-11-08 05:25:04 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * print_name - a function that prints name
+ * @f: variable to hold the string
+ * @name: name of the person
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
