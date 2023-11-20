d5f6d56021551e03cbe44a74544ffbc049d61a7f
“vintage254” <derricknjuguna414@gmail.com>
2023-11-20 00:50:34 -0800

    retirns number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/*
+ * listint_len - returns the number of elemnts 
+ * @h: pointer used to transverse
+ *
+ * Return: number of elemensts in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t ele = 0;
+
+	while (h)
+	{
+		ele ++;
+		h = h -> next;
+	}
+	return (ele);
+}
