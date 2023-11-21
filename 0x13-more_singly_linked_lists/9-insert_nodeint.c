e699084bb48f7a085db48c7140812fd8414f2436
“vintage254” <derricknjuguna414@gmail.com>
2023-11-21 01:05:24 -0800

    delete at desired position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,12 +14,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *new;
 	listint_t *temp = *head;
-	
+
 	new = malloc(sizeof(listint_t));
-	
+
 	if (!new || !head)
 		return (NULL);
-	
+
 	new->n = n;
 	new->next = NULL;
 	if (idx == 0)
