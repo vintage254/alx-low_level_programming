2ad77b274c39cb02f9161ab5697ad0741596fd7b
“vintage254” <derricknjuguna414@gmail.com>
2023-11-27 11:26:22 -0800

    create_file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,16 +15,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 	x = open(filename, O_RDONLY);
-	if ( x == -1)
+	if (x == -1)
 		return (0);
 	buf = malloc(sizeof(char) * (letters));
-	if ( !buf)
+	if (!buf)
 		return (0);
 
 	nrd = read(x, buf, letters);
 	nrw = write(STDOUT_FILENO, buf, nrd);
 
-	close (x);
-	free (buf);
+	close(x);
+	free(buf);
 	return (nrw);
 }
