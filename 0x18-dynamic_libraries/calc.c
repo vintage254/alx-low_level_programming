ab80ae607376cbcb968a11416c9b2d600cd5b4af
vintage254 <derricknjuguna414@gmail.com>
2024-01-15 10:48:19 -0800

    cpython

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+
+
+/**
+ * add - add two integers 
+ * @a: first integer
+ * @b: second integer
+ * return: results
+ */
+int add(int a, int b)
+{
+	int results;
+	results = a + b;
+	return (results);
+}
+/**
+ * sub - subtract two integers
+ * @a: first integer
+ * @b: second integer
+ * return: results
+ */
+int sub(int a, int b)
+{
+        int results;
+        results = a - b;
+        return (results);
+}
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * return: results
+ */
+int mul(int a, int b)
+{
+        int results;
+        results = a * b;
+        return (results);
+}
+/**
+ * mod - return modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ * return: results
+ */
+int mod(int a, int b)
+{
+        int results;
+        results = a % b;
+        return (results);
+}
---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * return: results
+ */
+int div(int a, int b)
+{
+        int results;
+        results = a / b;
+        return (results);
+}
