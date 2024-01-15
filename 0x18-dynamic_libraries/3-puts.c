301ef0b6b7f28aeca53ac4557528120b412e306d
vintage254 <derricknjuguna414@gmail.com>
2024-01-15 10:17:05 -0800

    script_bash

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-void _puts(char *str);
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void _puts(char *str)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
